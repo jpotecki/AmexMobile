42f611259104c01995d83e4ac70f4015b7935a3b
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2017-06-05 22:36:37 +0200

    First working version: Geolocation + interaction with Backend works

--- src/App.elm
+++ src/App.elm
@@ -1,37 +1,91 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Model exposing (..)
+import View exposing (..)
+import Types exposing (..)
 
-type alias Model = String
+import Html exposing (..)
+import WebSocket
+import Json.Decode exposing (..)
+import Table
+import Geocoding exposing (..)
+import Geolocation
+import Task exposing (..)
+import APIKey exposing (key)
 
+main : Program Never Model Msg
+main =
+  Html.program
+    { init = init []
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
-init : ( Model, Cmd Msg )
-init =
-    ( "hello" , Cmd.none )
-
-
-type Msg = NoOp
-
-
-view : Model -> Html Msg
-view model = div [] 
-                 [ text model ]
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of 
-    NoOp -> (model, Cmd.none)
+update msg model =
+  case msg of
+    SetQuery str ->
+       { model | query = str } ! []
+
+    Send ->
+      model ! [WebSocket.send "ws://localhost:8080" model.query]
+
+    NewMessage str -> 
+      case decodeString decodeStore str of
+        Ok s    -> 
+          { model | stores = s :: model.stores } ! []
+        Err err -> model ! []
+
+    SetTableState state ->
+      { model | tableState = state } ! []
+    
+    UsePosition ->
+      let
+        cmd = Geolocation.nowWith geoOpt |> Task.attempt GotLocation
+      in
+        model ! [ cmd ] 
+    GotLocation (Ok loc) ->
+      let
+        geo = (loc.latitude, loc.longitude)
+        cmd = reverseRequestForLatLng key geo
+                |> Geocoding.withLocationTypes [ Rooftop ]
+                |> Geocoding.withResultTypes geoResultTypes
+                |> Geocoding.sendReverseRequest MyReverseGeocoderResult
+      in
+        model ! [cmd]
+    GotLocation (Err err) ->
+      { model | errors = (toString err) :: model.errors } ! []
+
+    MyReverseGeocoderResult (Ok res) ->
+    case List.head res.results of
+      Nothing   -> { model | errors = "No Result" :: model.errors } ! []
+      Just xs   ->
+        { model | errors = (toString xs.formattedAddress) :: model.errors } ! []
+
+    MyReverseGeocoderResult (Err err) ->
+      { model | errors = (toString err) :: model.errors } ! []
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+  WebSocket.listen "ws://localhost:8080" NewMessage
 
 
+geoResultTypes : List Geocoding.ComponentType
+geoResultTypes = 
+  [ Geocoding.StreetAddress
+  , Geocoding.PostalCode
+  -- , Geocoding.Country
+  ]
 
-main : Program Never Model Msg 
-main = program  { init = init
-                , view = view
-                , update = update
-                , subscriptions = subscriptions
-                }
+geoOpt : Geolocation.Options
+geoOpt =
+  { enableHighAccuracy = True
+  , timeout = Just 20000
+  , maximumAge = Nothing
+  }
+
+-- {"zip":12627,"city":"Berlin","page":0,"url":"http://akzeptanz.amex-services.de/suche.php","distance":14,"firma_pattern":"BeginWi","business":"All","name":"Mc"}
